caa75a81a3776076b1d72f71731defd4f86f3f3c
Konstantinos Zafeiropoulos <konstantinos.zaf@gmail.com>
2025-07-13 15:08:00 +0300

    migrate to a fully async redis client

--- include/core/ports/cache_client_interface.h
+++ include/core/ports/cache_client_interface.h
@@ -2,10 +2,11 @@
 #include <string>
 #include <string_view>
 #include <optional>
+#include "core/ports/task.h"
 
 class ICacheClient {
 public:
     virtual ~ICacheClient() = default;
-    virtual std::optional<std::string> get(std::string_view key) = 0;
-    virtual void set(std::string_view key, std::string_view value, int ttl_seconds = 3600) = 0;
+    virtual async_task<std::optional<std::string>> get(std::string_view key) = 0;
+    virtual async_task<void> set(std::string_view key, std::string_view value, int ttl_seconds = 3600) = 0;
 };
